566ea9d8fb95840cea2d4fe2f9d6598f37212ce4
RILEYB <RILEYB@USL-1509.BIRLASOFT.COM>
2020-10-15 16:09:34 -0500

    Another commit

--- sss/Prj20ToString/src/App.java
+++ sss/Prj20ToString/src/App.java
@@ -1,8 +1,41 @@
+class Frog {
+	
+	
+	private int id;
+	private  String name;
+	
+	
+	
+	public Frog(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+
+
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(id).append(": ").append(name);
+		return sb.toString();
+	}
+}
+
 
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("hahdf");
+	
+		Object obj = new Object();
+		obj.toString();
+		
+		Frog frog1 = new Frog(7, "Freddy");
+		Frog frog2 = new Frog(5, "Roger");
+		
+		
+		// When you print an object it envokes the toString method.
+		System.out.println(frog1);
+		System.out.println(frog2);
 
 	}
 
